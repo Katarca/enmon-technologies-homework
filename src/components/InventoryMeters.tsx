1f93b68066d8c335a9a6503d6e97cde7660b6a49
Katarína Soušková <souskovakatarina@gmail.com>
2022-11-08 11:22:41 +0100

    sorting logic added

--- src/components/InventoryMeters.tsx
+++ src/components/InventoryMeters.tsx
@@ -2,47 +2,32 @@ import { CustomButton } from './Button'
 import { H1_Heading } from './Heading'
 import { P_BodyText } from './BodyText'
 import { UserStateContext } from '../context/UserContext'
+import { ReactComponent as arrowIcon } from '../icons/arrow-icon.svg'
 import { styles } from '../helpers/theme'
 import { urls } from '../helpers/urls'
 import { useComponentDidMount } from '../utils/useComponentDidMount'
 import { useNavigate } from 'react-router-dom'
 import React, { useContext, useState } from 'react'
 import axios from 'axios'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-type InventoryLocation = {
-  name: string
-  [key: string]: any
-}
-
-type InventoryLocationBuilding = {
-  name: string
-  [key: string]: any
-}
-
-type InventoryMeter = {
-  id: number
-  serial_number: null | string
-  meter_type: null | string
-  monitored_entity: string
-  inventory_location: null | InventoryLocation
-  inventory_location_building: null | InventoryLocationBuilding
-  accessibility: null | string
-  [key: string]: any
-}
+type SortField = 'id' | 'serial_number' | 'meter_type' | 'inventory_location_building.name'
+type SortOrder = 'ASC' | 'DESC'
 
 export const InventoryMeters = () => {
   const userContext = useContext(UserStateContext)
   const [loading, setLoading] = useState(false)
-  const [inventoryMeters, setInventoryMeters] = useState(null as null | InventoryMeter[])
+  const [inventoryMeters, setInventoryMeters] = useState(null as null | any)
+  const [sortField, setSortField] = useState<SortField>('id')
+  const [sortOrder, setSortOrder] = useState<SortOrder>('ASC')
 
   const navigate = useNavigate()
 
-  useComponentDidMount(async () => {
+  const fetchData = async (field: SortField, order: SortOrder) => {
     setLoading(true)
     try {
       const response = await axios.get(
-        `${process.env.REACT_APP_API_URL}api/inventory-meters?_sort=id:ASC`,
+        `${process.env.REACT_APP_API_URL}api/inventory-meters?_start=0&_limit=10&_sort=${field}:${order}`,
         {
           headers: {
             Authorization: `Bearer ${userContext.userJwt}`,
@@ -55,11 +40,29 @@ export const InventoryMeters = () => {
     } finally {
       setLoading(false)
     }
+  }
+
+  useComponentDidMount(async () => {
+    fetchData(sortField, sortOrder)
   })
 
   const handleLogout = () => {
     userContext.setUserJwt(null)
     navigate(urls.login)
+  }
+
+  const fetchSortedData = (clickedField: SortField) => {
+    if (clickedField !== sortField) {
+      setSortOrder('ASC')
+      setSortField(clickedField)
+      fetchData(clickedField, 'ASC')
+    } else if (clickedField === sortField && sortOrder === 'ASC') {
+      setSortOrder('DESC')
+      fetchData(clickedField, 'DESC')
+    } else {
+      setSortOrder('ASC')
+      fetchData(clickedField, 'ASC')
+    }
   }
 
   return (
@@ -75,17 +78,31 @@ export const InventoryMeters = () => {
           <Thead_InventoryThead>
             <tr>
               <th>
-                <Div_HeaderContainer>
+                <Div_HeaderContainer
+                  active={sortField === 'id'}
+                  sortOrder={sortOrder}
+                  onClick={() => fetchSortedData('id')}
+                >
                   <P_BodyText>ID</P_BodyText>
+                  <ArrowIcon />
                 </Div_HeaderContainer>
               </th>
               <th>
-                <Div_HeaderContainer>
+                <Div_HeaderContainer
+                  active={sortField === 'serial_number'}
+                  sortOrder={sortOrder}
+                  onClick={() => fetchSortedData('serial_number')}
+                >
                   <P_BodyText>Serial key</P_BodyText>
+                  <ArrowIcon />
                 </Div_HeaderContainer>
               </th>
               <th>
-                <Div_HeaderContainer>
+                <Div_HeaderContainer
+                  active={sortField === 'meter_type'}
+                  sortOrder={sortOrder}
+                  onClick={() => fetchSortedData('meter_type')}
+                >
                   <P_BodyText>Meter Type</P_BodyText>
                 </Div_HeaderContainer>
               </th>
@@ -100,8 +117,13 @@ export const InventoryMeters = () => {
                 </Div_HeaderContainer>
               </th>
               <th>
-                <Div_HeaderContainer>
+                <Div_HeaderContainer
+                  active={sortField === 'inventory_location_building.name'}
+                  sortOrder={sortOrder}
+                  onClick={() => fetchSortedData('inventory_location_building.name')}
+                >
                   <P_BodyText>Building</P_BodyText>
+                  <ArrowIcon />
                 </Div_HeaderContainer>
               </th>
               <th>
@@ -111,29 +133,29 @@ export const InventoryMeters = () => {
               </th>
             </tr>
           </Thead_InventoryThead>
-          {loading ? (
-            <P_BodyText>Loading data...</P_BodyText>
-          ) : (
-            <tbody>
-              {inventoryMeters?.map(meter => (
-                <tr key={meter.id}>
-                  <Td_InventoryTd>{meter.id}</Td_InventoryTd>
-                  <Td_InventoryTd>{meter.serial_number}</Td_InventoryTd>
-                  <Td_InventoryTd>{meter.meter_type}</Td_InventoryTd>
-                  <Td_InventoryTd>{meter.monitored_entity}</Td_InventoryTd>
-                  <Td_InventoryTd>
-                    {meter.inventory_location ? meter.inventory_location.name : null}
-                  </Td_InventoryTd>
-                  <Td_InventoryTd>
-                    {meter.inventory_location_building
-                      ? meter.inventory_location_building.name
-                      : null}
-                  </Td_InventoryTd>
-                  <Td_InventoryTd>{meter.accessibility}</Td_InventoryTd>
-                </tr>
-              ))}
-            </tbody>
-          )}
+          <tbody>
+            {inventoryMeters?.map((meter: any) => (
+              <tr key={meter.id}>
+                <Td_InventoryTd>{meter.id}</Td_InventoryTd>
+                <Td_InventoryTd>{meter.serial_number}</Td_InventoryTd>
+                <Td_InventoryTd>{meter.meter_type}</Td_InventoryTd>
+                <Td_InventoryTd>{meter.monitored_entity}</Td_InventoryTd>
+                <Td_InventoryTd>
+                  {meter.inventory_location ? meter.inventory_location.name : null}
+                </Td_InventoryTd>
+                <Td_InventoryTd>
+                  {meter.inventory_location_building
+                    ? meter.inventory_location_building.name
+                    : null}
+                </Td_InventoryTd>
+                <Td_InventoryTd>
+                  {meter.accessibility?.split(';').map((accessibility: any, i: number) => (
+                    <Span_AccessibilitySpan key={i}>{accessibility}</Span_AccessibilitySpan>
+                  ))}
+                </Td_InventoryTd>
+              </tr>
+            ))}
+          </tbody>
         </Table_InventoryTable>
       </Div_Box>
     </>
@@ -155,12 +177,40 @@ const Thead_InventoryThead = styled.thead`
   background-color: ${styles.colors.grey50};
 `
 
-const Div_HeaderContainer = styled.div`
+const Div_HeaderContainer = styled.div<{
+  active?: boolean
+  sortOrder?: SortOrder
+}>`
   display: flex;
   padding: ${styles.spacing.l} ${styles.spacing.m};
+  justify-content: space-between;
+  cursor: pointer;
+  ${({ active }) =>
+    active &&
+    css`
+      background-color: ${styles.colors.grey100};
+    `}
+  ${({ active, sortOrder }) =>
+    active &&
+    sortOrder === 'DESC' &&
+    css`
+      & > svg {
+        transform: rotate(0deg);
+      }
+    `}
 `
 const Td_InventoryTd = styled.td`
   font-size: ${styles.fontSize.s};
   color: ${styles.colors.black};
   padding: ${styles.spacing.m} ${styles.spacing.l};
+`
+const Span_AccessibilitySpan = styled.span`
+  margin-right: ${styles.spacing.xs};
+  display: inline-block;
+`
+const ArrowIcon = styled(arrowIcon)`
+  width: 10px;
+  fill: ${styles.colors.grey300};
+  margin-left: ${styles.spacing.xs};
+  transform: rotate(-180deg);
 `
